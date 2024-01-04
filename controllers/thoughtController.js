d360f0745d1f81d8bd868ca5c65495f7a0a6c5b0
Joshua Kennedy-Dedam <joshuajordyn@hotmail.com>
2024-01-04 17:42:35 -0400

    Got the reactions working, all done just need to make the walk through video

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -32,5 +32,48 @@ module.exports = {
         } catch (err) {
             res.status(500).json(err);
         }
+    },
+    
+    async DeleteThought(req,res) {
+        try {
+            const thought = await Thought.findByIdAndDelete(req.params.id)
+            res.json(`${thought.thoughtText} has been deleted`);
+          } catch (err) {
+            res.status(500).json(err)
+          }
+    },
+
+    async UpdateThought(req,res) {
+        try {
+            const thought = await Thought.findByIdAndUpdate(req.params.id, req.body);
+            res.json("Thought has been updated!")
+        }   catch (err) {
+            res.status(500).json(err)
+          }
+    },
+
+
+    async CreateReaction(req,res) {
+        try {
+            const reaction = await Thought.findByIdAndUpdate(req.params.id, {reactions:req.body});
+            res.json(`${reaction} added`)
+        } catch (err) {
+            res.status(500).json(err)
+          }
+    }  ,
+
+    async DeleteReaction(req,res) {
+        try {
+            const reaction = await Thought.findByIdAndUpdate(req.params.id, 
+                {$pull:{reactions:{_id: req.params.reactionId}}},
+                {new:true});
+                
+            res.json(`reaction has been deleted`);
+        } catch {
+            res.status(500).json(err);
+        }
     }
+
+    
+
 }
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,8 +1,18 @@
 const router = require("express").Router();
-const {GetAllThoughts,CreateThought,GetSingleThought} = require("../../controllers/thoughtController")
+const { GetAllThoughts,
+        CreateThought,
+        GetSingleThought,
+        DeleteThought,
+        UpdateThought,
+        CreateReaction,
+        DeleteReaction} = require("../../controllers/thoughtController")
 
 router.route("/").get(GetAllThoughts).post(CreateThought);
 
-router.route("/:id").get(GetSingleThought);
+router.route("/:id").get(GetSingleThought).delete(DeleteThought).put(UpdateThought);
+
+router.route("/:id/reactions").post(CreateReaction);
+
+router.route("/:id/reactions/:reactionId").delete(DeleteReaction);
 
 module.exports = router;
